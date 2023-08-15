334d8bb03824a3c16fec768c85bef5eaaf342343
Lazar Nikolov <hello@nikolovlazar.com>
2023-08-15 17:47:34 -0400

    lesson 13 code

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,24 @@
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
+
+export async function get(context: APIContext) {
+  const blog = await getCollection('blog');
+
+  return rss({
+    title: "Lazar Nikolov's Blog",
+    description: 'I write about web development!',
+    site: context.site?.toString() || 'https://nikolovlazar.com',
+    items: blog.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.date,
+      link: `/blog/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body)),
+    })),
+  });
+}
