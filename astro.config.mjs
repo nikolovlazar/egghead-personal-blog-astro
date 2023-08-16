4b3ffa7b2e9a5a126e120a781a4029e1f77640f6
Lazar Nikolov <hello@nikolovlazar.com>
2023-08-16 19:22:02 -0400

    lesson 17 code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,35 +2,40 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nikolovlazar.com',
-  integrations: [tailwind(), sitemap({
-    filter: page => !/https:\/\/nikolovlazar\.com\/blog\/[0-9]+/.test(page),
-    customPages: ['https://nikolovlazar.com/discord', 'https://nikolovlazar.com/twitch', 'https://nikolovlazar.com/twitter'],
-    changefreq: 'weekly',
-    lastmod: new Date(),
-    priority: 0.85,
-    serialize: item => {
-      // Remove trailing slashes
-      if (item.url.at(-1) === '/') {
-        item.url = item.url.slice(0, -1);
-      }
-      return item;
-    },
-    i18n: {
-      defaultLocale: 'en',
-      // Just an example, we're not actually implementing i18n
-      locales: {
-        en: 'en-US',
-        es: 'es-ES',
-        fr: 'fr-CA'
-      }
-    }
-  }), react()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => !/https:\/\/nikolovlazar\.com\/blog\/[0-9]+/.test(page),
+      customPages: [
+        'https://nikolovlazar.com/discord',
+        'https://nikolovlazar.com/twitch',
+        'https://nikolovlazar.com/twitter',
+      ],
+      changefreq: 'weekly',
+      lastmod: new Date(),
+      priority: 0.85,
+      serialize: (item) => {
+        // Remove trailing slashes
+        if (item.url.at(-1) === '/') {
+          item.url = item.url.slice(0, -1);
+        }
+        return item;
+      },
+      i18n: {
+        defaultLocale: 'en',
+        // Just an example, we're not actually implementing i18n
+        locales: {
+          en: 'en-US',
+          es: 'es-ES',
+          fr: 'fr-CA',
+        },
+      },
+    }),
+  ],
   experimental: {
-    assets: true
-  }
+    assets: true,
+  },
 });
--- src/components/PostLike.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostLike() {
-  return (
-    <button className='px-3 py-1 rounded-md bg-blue-500 text-white'>
-      Like
-    </button>
-  );
-}
